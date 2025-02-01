5ed5f26456c3541ff968aa0fdb2ccf9dff9131d1
axgomez2 <ax.gomez2@gmail.com>
2025-02-01 15:09:35 -0300

    correção vite 2

--- vite.config.js
+++ vite.config.js
@@ -4,26 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js'
-            ],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
-    server: {
-        hmr: {
-            host: 'localhost',
-        },
-    },
     build: {
-        // Adicione estas configurações
         manifest: true,
         outDir: 'public/build',
-        rollupOptions: {
-            output: {
-                manualChunks: undefined
-            }
-        }
-    }
+    },
 });
